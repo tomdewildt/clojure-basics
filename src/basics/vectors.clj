b9e30f2620c3ae5da7fea3dc3831a3c270f4dc35
tomdewildt <t.l.dewildt@gmail.com>
2020-04-11 23:53:38 +0200

    Add vectors basics

---
+++ src/basics/vectors.clj
@@ -0,0 +1,19 @@
+(ns basics.vectors
+  (:gen-class))
+
+(defn main
+  []
+  ;; Define vector variable
+  (def vector-variable (vector 1 2 3))
+
+  ;; Index
+  (println "Index:" (get vector-variable 2))
+
+  ;; Append element
+  (println "Append element:" (conj vector-variable 4))
+
+  ;; Remove first element
+  (println "Remove first element:" (pop vector-variable))
+
+  ;; Get subvector
+  (println "Get subvector:" (subvec vector-variable 0 2)))
